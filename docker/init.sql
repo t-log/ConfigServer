52aadb0ca445a07acd9132f080b9491c3fbd1cdf
limitlesslogger <anushapaul789@gmail.com>
2025-03-30 13:15:39 +0530

    sql script changes

--- docker/init.sql
+++ docker/init.sql
@@ -1,30 +1,30 @@
-CREATE TABLE IF NOT EXISTS public.organizations
+CREATE TABLE IF NOT EXISTS public.organisations
 (
-    organization_id text COLLATE pg_catalog."default" NOT NULL,
+    organisation_id text COLLATE pg_catalog."default" NOT NULL,
     name text COLLATE pg_catalog."default",
     contact_name text COLLATE pg_catalog."default",
     contact_email text COLLATE pg_catalog."default",
     contact_phone text COLLATE pg_catalog."default",
-    CONSTRAINT organizations_pkey PRIMARY KEY (organization_id)
+    CONSTRAINT organisations_pkey PRIMARY KEY (organisation_id)
 )
 
     TABLESPACE pg_default;
 
-ALTER TABLE public.organizations
+ALTER TABLE public.organisations
     OWNER to postgres;
 
 
 CREATE TABLE IF NOT EXISTS public.licenses
 (
     license_id text COLLATE pg_catalog."default" NOT NULL,
-    organization_id text COLLATE pg_catalog."default" NOT NULL,
+    organisation_id text COLLATE pg_catalog."default" NOT NULL,
     description text COLLATE pg_catalog."default",
     product_name text COLLATE pg_catalog."default" NOT NULL,
     license_type text COLLATE pg_catalog."default" NOT NULL,
     comment text COLLATE pg_catalog."default",
     CONSTRAINT licenses_pkey PRIMARY KEY (license_id),
-    CONSTRAINT licenses_organization_id_fkey FOREIGN KEY (organization_id)
-    REFERENCES public.organizations (organization_id) MATCH SIMPLE
+    CONSTRAINT licenses_organisation_id_fkey FOREIGN KEY (organisation_id)
+    REFERENCES public.organisations (organisation_id) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION
     NOT VALID
